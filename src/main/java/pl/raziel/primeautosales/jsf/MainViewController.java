0e17aa602522bff67f29312d4c48c96f58958ce1
mietekzul <noname_18@wp.pl>
2016-10-03 23:59:14 +0200

    Added TabView component & added rendering.

--- src/main/java/pl/raziel/primeautosales/jsf/MainViewController.java
+++ src/main/java/pl/raziel/primeautosales/jsf/MainViewController.java
@@ -5,9 +5,12 @@
  */
 package pl.raziel.primeautosales.jsf;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import org.primefaces.event.ResizeEvent;
+import pl.raziel.primeautosales.models.Car;
 
 /**
  *
@@ -17,13 +20,60 @@ import org.primefaces.event.ResizeEvent;
 @RequestScoped
 public class MainViewController {
 
+    private String selectionType;
+    private List<Car> automobiles = new ArrayList<>();
+
     /**
      * Creates a new instance of MainViewController
      */
     public MainViewController() {
     }
-    
-    public void layoutResizeEvent(ResizeEvent event){
+
+    public void layoutResizeEvent(ResizeEvent event) {
         System.out.println("To gówno zmienia rozmiar: " + event.getComponent());
+    }
+
+    public String navigateToCar() {
+        selectionType = "CAR";
+        return "automobile";
+    }
+
+    public String navigateToPickup() {
+        selectionType = "TRUCK";
+        return "automobile";
+    }
+
+    public String navigateToSuv() {
+        selectionType = "SUV";
+        return "automobile";
+    }
+
+    public String getSelectionType() {
+        return selectionType;
+    }
+
+    public void setSelectionType(String selectionType) {
+        this.selectionType = selectionType;
+    }
+
+    public List<Car> getAutomobiles() {
+        Car car1 = new Car();
+        car1.setMake("Juneau");
+        car1.setModel("Turbo");
+        Car car2 = new Car();
+        car2.setMake("Juneau");
+        car2.setModel("Rocket");
+        Car car3 = new Car();
+        car3.setMake("Acme");
+        car3.setModel("Jet");
+        automobiles = new ArrayList();
+        automobiles.add(car1);
+        automobiles.add(car2);
+        automobiles.add(car3);
+        return automobiles;
+    }
+
+    public void setAutomobiles(List<Car> automobiles) {
+        this.automobiles = automobiles;
     }
 }
---
+++ src/main/java/pl/raziel/primeautosales/models/Car.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.raziel.primeautosales.models;
+
+/**
+ *
+ * @author RazieL
+ */
+public class Car {
+    private String make;
+    private String model;
+    private String description;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+}
